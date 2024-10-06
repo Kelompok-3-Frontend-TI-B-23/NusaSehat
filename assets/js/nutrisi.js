94bc53bfa6b8a2dd7829ea1e55ff1c0dd856dda6
Raymond Euginio <rayeuginio@gmail.com>
2024-10-06 22:04:33 +0700

    menambahkan search bar di hero dan bikin fungsi popup saat infocontainer dipencet

--- assets/js/nutrisi.js
+++ assets/js/nutrisi.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
     const infos = [
-        { title: "Rendang", img: "../assets/images/rendang.jpg" },
-        { title: "Sate Ayam", img: "../assets/images/sate.jpg" },
-        { title: "Nasi Goreng", img: "../assets/images/nasgor.jpg" },
-        { title: "Batagor", img: "../assets/images/bataghor.jpg" },
-        { title: "Ayam Geprek", img: "../assets/images/geprek.png" }
+        { title: "Rendang", img: "../assets/images/rendang.jpg", desc: "Rendang is a slow-cooked dry curry from Indonesia, rich in flavor and made from beef." },
+        { title: "Sate Ayam", img: "../assets/images/sate.jpg", desc: "Sate Ayam is grilled chicken skewers served with peanut sauce, a popular street food in Indonesia." },
+        { title: "Nasi Goreng", img: "../assets/images/nasgor.jpg", desc: "Nasi Goreng is Indonesian-style fried rice, commonly served with a fried egg on top." },
+        { title: "Batagor", img: "../assets/images/bataghor.jpg", desc: "Batagor is a fried fish dumpling served with peanut sauce, originating from West Java, Indonesia." },
+        { title: "Ayam Geprek", img: "../assets/images/geprek.png", desc: "Ayam Geprek is a smashed fried chicken, served with spicy sambal, originating from Yogyakarta." }
     ];
 
     const infoContainer = $('.infoContainer');
 
     infos.forEach(info => {
         const infoCard = `
-            <div class="info-card">
+            <div class="info-card" data-title="${info.title}" data-img="${info.img}" data-desc="${info.desc}">
                 <div class="image-wrapper">
                     <img src="${info.img}" alt="Info Image" />
                 </div>
@@ -21,5 +21,50 @@ $(document).ready(function() {
             </div>
         `;
         infoContainer.append(infoCard);
+    });
+
+    // Search feature
+    $('#search-button').on('click', function() {
+        const searchValue = $('#search-input').val().toLowerCase();
+        $('.info-card').each(function() {
+            const title = $(this).data('title').toLowerCase();
+            if (title.includes(searchValue)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    });
+
+    $('#search-input').on('keypress', function(event) {
+        if (event.which === 13) { // Enter key
+            $('#search-button').click();
+        }
+    });
+
+    // Popup feature
+    const popup = $('<div id="popup"><div id="popup-content"><span id="popup-close">&times;</span><img id="popup-img" /><h3 id="popup-title"></h3><p id="popup-desc"></p></div></div>');
+    $('body').append(popup);
+
+    $('.info-card').on('click', function() {
+        const imgSrc = $(this).data('img');
+        const title = $(this).data('title');
+        const desc = $(this).data('desc');
+
+        $('#popup-img').attr('src', imgSrc);
+        $('#popup-title').text(title);
+        $('#popup-desc').text(desc);
+
+        $('#popup').fadeIn();
+    });
+
+    $('#popup-close').on('click', function() {
+        $('#popup').fadeOut();
+    });
+
+    $('#popup').on('click', function(e) {
+        if (e.target === this) {
+            $('#popup').fadeOut();
+        }
     });
 });
